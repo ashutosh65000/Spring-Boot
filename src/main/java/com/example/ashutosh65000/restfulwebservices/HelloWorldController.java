5b9d61acd8b9c6e4f90fef420017f4084cce2b79
Ashutosh Srivastava <ashutosh65000@outlook.com>
2020-12-13 23:17:05 +0530

    Enhancing the Hello World Service to return a Bean

---
+++ src/main/java/com/example/ashutosh65000/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.example.ashutosh65000.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//need to tell this is a Controller 
+@RestController
+public class HelloWorldController {
+	
+	/*
+	 * Step 1. Telling this is a GET method
+	 * 			a) using the annotation @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	 * 			b) using the shorthand annotation @GetMapping(path="/hello-world")
+	 * Step 2. The URI is /hello-world
+	 * Step 3. What it should return back as the response?
+	
+	 */
+	
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hey Ashutosh! Hello";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hey Ashutosh! Hello");
+	}
+}
---
+++ src/test/java/com/example/ashutosh65000/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ashutosh65000.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
